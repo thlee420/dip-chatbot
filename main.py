34725895c1902120062640df6bc5dff72b21d14d
thlee420 <70680771+thlee420@users.noreply.github.com>
2024-08-09 15:45:12 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -7,25 +7,37 @@ st.title(":writing_hand: :robot_face: :thumbsup: ChatBot :kr:")
 
 col1, col2 = st.columns([1,4])
 
-col1.markdown("만든이")
+col1.markdown("#### 만든이")
 col1.markdown(":man: [Taehee](https:researchgate.net/profile/Taehee-Lee)")
 col1.markdown(":email: [이메일 보내기](mailto:thlee420@gmail.com)")
 
+col1.markdown("#### 지도자")
+col1.markdown(":man: [Teddy](https:https://github.com/teddylee777)")
+col1.markdown(":email: [이메일 보내기](mailto:teddy777420@gmail.com)")
+
+
 col2.markdown(
         """
-    OpenAI API 키 발급 방법은 아래 링크를 참고해 주세요!
+    #### OpenAI API 키 발급 방법은 아래 링크를 참고해 주세요!
     * [발급방법](https://wikidocs.net/233342)
+
+    #### 실시간 검색을 위한 TAVILY_API_KEY 키 발급 방법은 아래 링크를 참고해 주세요!
+    * [발급방법](https://app.tavily.com/home)
     """
     )
 
-
 # API 키 입력
-api_key = col2.text_input("API 키 입력", type="password")
-
-    # 설정 확인 버튼
+api_key = col2.text_input("OPENAI API 키 입력(ChatGPT)", type="password")
+# API 키 입력
+search_api_key = col2.text_input("Tavily Search API 키 입력(검색용)", type="password")
+# 설정 확인 버튼
 confirm_btn = col2.button("설정하기", key="api_key")
-
 if confirm_btn:
-    os.environ["OPENAI_API_KEY"] = api_key
-    col2.write(f"API 키가 설정되었습니다: `{api_key[:15]}************`")
-
+    if api_key:
+        os.environ["OPENAI_API_KEY"] = api_key
+        col2.write(f"OPENAI API 키가 설정되었습니다: `{api_key[:15]}************`")
+    if search_api_key:
+        os.environ["TAVILY_API_KEY"] = search_api_key
+        col2.write(
+            f"TAVILPY API 키가 설정되었습니다: `{search_api_key[:15]}************`"
+        )
